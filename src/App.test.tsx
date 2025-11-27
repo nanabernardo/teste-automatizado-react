469b6dfdeac4e6298ae33a0ea3365323a5da6209
nanabernardo <nanabernardo@gmail.com>
2025-11-27 19:17:10 -0300

    first unit test exemple

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,9 @@
+const soma = (a: number, b: number) => {
+  return a + b;
+};
+
 describe("Testes matemáticos", () => {
-    test('Deve somar um número corretamente', () => {
-        
-    })
+  test("Deve somar um número corretamente", () => {
+    expect(soma(2, 2)).toBe(4);
+  });
 });
