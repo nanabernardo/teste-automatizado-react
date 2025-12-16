54751a36da7dea865caee08d8266bab1d648f6eb
nanabernardo <nanabernardo@gmail.com>
2025-12-16 20:54:16 -0300

    docs: add explanatory comments

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ const soma = (a: number, b: number) => {
   return a + b;
 };
 
-describe("Testes matemáticos", () => {
+describe("Testes matemáticos", () => { // "pasta" de testes
   test("Deve somar um número corretamente", () => {
+    // assertion - expectativa
     expect(soma(2, 2)).toBe(4);
   });
 });
